32047e3b95b7323d7e09de850c970399d92f7d82
Isabella Galbero <isagalbero@gmail.com>
2019-11-28 16:40:36 -0300

    Diferença entre float e double em C#

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            //aqui é um double pq depende da precisão
             var numbers = new[] {12.7, 10.3, 6.11, 4.1};
             var result = numbers[0];
             result = result + numbers[1];
