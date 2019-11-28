55e7433af0a9e37e71ca3fd1068fda93e8ca87db
Isabella Galbero <isagalbero@gmail.com>
2019-11-28 20:24:30 -0300

    Exercício de array

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,8 +9,8 @@ namespace GradeBook
             //aqui é um double pq depende da precisão
             var numbers = new[] {12.7, 10.3, 6.11, 4.1};
             var result = numbers[0];
-            result = result + numbers[1];
-            result = result + numbers[2];
+            result += numbers[1];
+            result += numbers[2];
             Console.WriteLine(result);
 
 
