8d10819c6ad32f8e6fd5a4bc8649bf29493e4ee7
Isabella Galbero <isagalbero@gmail.com>
2019-11-28 16:31:35 -0300

    Inserindo funções básicas

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,13 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+            var result = numbers[0];
+            result = result + numbers[1];
+            result = result + numbers[2];
+            Console.WriteLine(result);
+
+
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
