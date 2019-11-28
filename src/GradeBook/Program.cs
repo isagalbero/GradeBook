d14ce0df7abad6c2e49a1939f60a2f1fa3b6fa3f
Isabella Galbero <isagalbero@gmail.com>
2019-11-28 20:32:24 -0300

    LOop com foreach - exercicio

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,9 +8,11 @@ namespace GradeBook
         {
             //aqui é um double pq depende da precisão
             var numbers = new[] {12.7, 10.3, 6.11, 4.1};
-            var result = numbers[0];
-            result += numbers[1];
-            result += numbers[2];
+            var result = 0.0;
+            foreach(var number in numbers)
+            {
+                result += number;
+            }
             Console.WriteLine(result);
 
 
