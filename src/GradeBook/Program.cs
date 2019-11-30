e4f425c796575b5be118983153ff4e7cca12260f
Isabella Galbero <isagalbero@gmail.com>
2019-11-30 11:12:55 -0300

    Usando .Count em C#

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,13 +7,15 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            //aqui é um double pq depende da precisão
-            var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            grades.Add(56.1);
             var result = 0.0;
-            foreach(var number in numbers)
+            foreach(var number in grades)
             {
                 result += number;
+                
             }
+            result /= grades.Count;
             Console.WriteLine(result);
 
 
