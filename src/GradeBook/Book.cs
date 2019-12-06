a90f9a29059fad1a598a2dd42e65859020d40c4e
Isabella Galbero <isagalbero@gmail.com>
2019-12-05 21:23:06 -0300

    Criando classe desafio Showstatistcs()

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -4,9 +4,17 @@ namespace GradeBook
 {
   class Book
   {
+    public Book(string name)
+    {
+      grades = new List<double>();
+      this.name = name;
+    }
     public void AddGrade(double grade)
     {
-          
+      grades.Add(grade);
     }
+
+    private List<double> grades;
+    private string name;
   }
 }
