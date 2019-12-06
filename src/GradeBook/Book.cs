62e8658eb194cfd7039c42abd8fb3a1d8134008e
Isabella Galbero <isagalbero@gmail.com>
2019-12-05 21:37:57 -0300

    Exercício encapsulamento

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -14,7 +15,40 @@ namespace GradeBook
       grades.Add(grade);
     }
 
+    public void Showstatistics()
+    {
+      var result = 0.0;
+          var highGrade = double.MinValue;
+          var lowGrade = double.MaxValue;
+
+          foreach(var number in grades)
+          {
+            highGrade = Math.Max(number, highGrade);
+            lowGrade = Math.Min(number, lowGrade);
+            result += number;
+              
+          }
+          result /= grades.Count;
+
+          Console.WriteLine($"O valor da média é {result:N2}");
+          Console.WriteLine($"Valor máximo: {highGrade}");
+          Console.WriteLine($"Valor máximo: {lowGrade}");
+
+          /*
+          if(args.Length > 0)
+          {
+            Console.WriteLine($"Hello, {args[0]}!");
+          }
+          else
+          {
+            Console.WriteLine("Hello");
+          }
+          */
+    }
+
     private List<double> grades;
     private string name;
+
+        
   }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,40 +7,12 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-          var book = new Book();
-          book.AddGrade(89.1);  
+          var book = new Book("Notas de ...");
+          book.AddGrade(89.1);
+          book.AddGrade(90.5);
+          book.AddGrade(77.5);
+          book.Showstatistics();  
 
-          var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
-          grades.Add(56.1);
-
-          var result = 0.0;
-          var highGrade = double.MinValue;
-          var lowGrade = double.MaxValue;
-
-          foreach(var number in grades)
-          {
-            highGrade = Math.Max(number, highGrade);
-            lowGrade = Math.Min(number, lowGrade);
-            result += number;
-              
-          }
-          result /= grades.Count;
-
-          Console.WriteLine($"O valor da média é {result:N2}");
-          Console.WriteLine($"Valor máximo: {highGrade}");
-          Console.WriteLine($"Valor máximo: {lowGrade}");
-
-
-          if(args.Length > 0)
-          {
-            Console.WriteLine($"Hello, {args[0]}!");
-          }
-          else
-          {
-            Console.WriteLine("Hello");
-          }
         }
-        
-        
     }
 }
