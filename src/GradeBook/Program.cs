ee144407d8fac06f792842419c023c4dcd280fdd
Isabella Galbero <isagalbero@gmail.com>
2019-12-05 21:13:38 -0300

    Computando os valores max. min.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,16 +9,26 @@ namespace GradeBook
         {
           var book = new Book();
           book.AddGrade(89.1);  
+
           var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
           grades.Add(56.1);
+
           var result = 0.0;
+          var highGrade = double.MinValue;
+          var lowGrade = double.MaxValue;
+
           foreach(var number in grades)
           {
-              result += number;
+            highGrade = Math.Max(number, highGrade);
+            lowGrade = Math.Min(number, lowGrade);
+            result += number;
               
           }
           result /= grades.Count;
+
           Console.WriteLine($"O valor da média é {result:N2}");
+          Console.WriteLine($"Valor máximo: {highGrade}");
+          Console.WriteLine($"Valor máximo: {lowGrade}");
 
 
           if(args.Length > 0)
